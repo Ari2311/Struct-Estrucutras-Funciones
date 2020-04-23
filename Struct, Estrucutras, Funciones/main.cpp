84f26a701eb644dce95dbac1f0eddcab9981e91d
Ari <Ari>
2020-04-23 15:21:56 -0500

    Initial Commit

---
+++ Struct, Estrucutras, Funciones/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Struct, Estrucutras, Funciones
+//
+//  Created by Ariadna on 23/04/20.
+//  Copyright © 2020 Ariadna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
